d509ee08616d8b532f93d30bdda731944bba9a94
Brett <brett.g.hays.coding@gmail.com>
2017-12-22 23:15:29 -0700

    start of step 4

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ConfirmModal from './subcomponents/ConfirmModal';
 
 // import axios
+import axios from 'axios'
 
 class Add extends Component {
     constructor(){
@@ -19,6 +20,16 @@ class Add extends Component {
     }
 
     // insert post function
+    post(){
+        let body = {
+            title: this.state.title,
+            subTitle: this.state.subTitle,
+            image: this.state.image,
+            text: this.state.text
+        }
+        axios.post(`/api/blogs`, body)
+        .then(res => console.log(res))
+    }
     
     
     render() {
